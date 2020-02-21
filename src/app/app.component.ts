10ff5a17d73289e844ceb17375f9cf29bf62b7f7
amr-ahmed1 <amr.ahmed1@ibm.com>
2020-02-21 21:01:41 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, Injectable, ReflectiveInjector } from "@angular/core";
+import { WikipediaService } from "./wikipedia.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  pages = [];
+
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    if (term && term.length) {
+      this.wikipedia.search(term).subscribe((response: any) => {
+        this.pages = response.query.search;
+      });
+    } else {
+      this.pages = [];
+    }
+  }
+}
